fab47c554be3b217fea214fbb55338104c2ce9bb
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-06-29 13:56:47 -0500

    added weather api call

--- client/src/components/WeatherBox.component.js
+++ client/src/components/WeatherBox.component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -8,6 +8,35 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 function WeatherBox() {
+  const [cityName, setCityName] = useState("");
+  const [cityData, setCityData] = useState({
+    name: "",
+    temperature: 0,
+    humidity: 0,
+    time: "",
+  });
+
+  const handleChange = (event) => {
+    setCityName(event.target.value);
+    console.log(cityName);
+  };
+
+  const searchCity = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8c3d8bf818c52d5d46964ed0d3c7b483`
+    );
+    const json = await response.json();
+    console.log(json);
+    const d = Date();
+    const F = json.main.temp * (9 / 5) - 459.67;
+    setCityData({
+      name: json.name,
+      temperature: F.toFixed(0),
+      humidity: json.main.humidity,
+      time: d.toString(),
+    });
+  };
+
   return (
     <div>
       <Container className="border mt-3">
@@ -25,28 +54,34 @@ function WeatherBox() {
               placeholder="Ex. - Houston"
               aria-label="Ex. - Houston"
               aria-describedby="basic-addon2"
+              onChange={handleChange}
             />
             <InputGroup.Append>
-              <Button variant="primary">Search</Button>
+              <Button variant="primary" onClick={searchCity}>
+                Search
+              </Button>
             </InputGroup.Append>
           </InputGroup>
         </Row>
-        <Row className="justify-content-center">
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://images.unsplash.com/photo-1534794048419-48e110dca88e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=948&q=80"
-            />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Row>
+        {cityData.name && (
+          <Row className="justify-content-center">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://images.unsplash.com/photo-1534794048419-48e110dca88e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=948&q=80"
+              />
+              <Card.Body>
+                <Card.Title>{cityData.name}</Card.Title>
+                <Card.Text>
+                  <strong>Temperature:</strong> {cityData.temperature}°F
+                  <br />
+                  <strong>Humidity:</strong> {cityData.humidity}%
+                </Card.Text>
+                <Button variant="primary">Save</Button>
+              </Card.Body>
+            </Card>
+          </Row>
+        )}
       </Container>
     </div>
   );
